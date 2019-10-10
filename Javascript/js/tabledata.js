37ff2460cf90b93a535d49f2d56a5de21a2534bd
VB1987 <53115589+VB1987@users.noreply.github.com>
2019-10-10 13:57:55 +0200

    Add files via upload

---
+++ Javascript/js/tabledata.js
@@ -0,0 +1,8 @@
+var persons = [
+	{"firstname":"Nel", "lastname":"De Beer", "age":9, "location":"Weert"},
+	{"firstname":"Bob", "lastname":"Janssen", "age":33, "location":"Eindhoven"},
+	{"firstname":"Beatrix", "lastname":"Peperkamp", "age":76, "location":"Nijmegen"},
+	{"firstname":"Frans", "lastname":"Vermeulen", "age":41, "location":"Rotterdam"},
+	{"firstname":"Kaatje", "lastname":"verstappen", "age":23, "location":"Venlo"},
+	{"firstname":"Japie", "lastname":"De Binck", "age":29, "location":"Amsterdam"}
+];
